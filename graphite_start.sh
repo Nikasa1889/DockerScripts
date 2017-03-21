f5fdcb4f9d5290f7bda9b7ccd2e83ddbabe76e12
Nikasa <nikasa.1889@gmail.com>
2017-03-21 14:19:19 +0100

    Add graphite_start.sh to start graphite docker image

---
+++ graphite_start.sh
@@ -0,0 +1,12 @@
+sudo docker run -d \
+  --name graphite \
+  -p 8080:80 \
+  -p 2003-2004:2003-2004\
+  -p 2023-2024:2023-2024\
+  -p 8125:8125/udp\
+  -p 8126:8126\
+  -v /home/nikasa/Graphite/.htpasswd:/etc/nginx/.htpasswd\
+  -v /home/nikasa/Graphite/data:/opt/graphite/storage\
+  -v /home/nikasa/Graphite/configs:/opt/graphite/conf\
+  -v /home/nikasa/Graphite/statsd:/opt/statsd\
+  sitespeedio/graphite
