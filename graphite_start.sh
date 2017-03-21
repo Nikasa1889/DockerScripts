e39b5e612e57256299e31a2f1a5b7600aa3ef611
Nikasa <nikasa.1889@gmail.com>
2017-03-21 17:30:03 +0100

    Change the docker image to my version of graphite docker, map the storage-conf file

--- graphite_start.sh
+++ graphite_start.sh
@@ -1,12 +1,13 @@
 sudo docker run -d \
   --name graphite \
   -p 8080:80\
-  -p 2003-2004:2003-2004\
+  -p 2003:2003\
+  -p 2004:2004\
   -p 2023-2024:2023-2024\
   -p 8125:8125/udp\
   -p 8126:8126\
   -v /home/nikasa/Graphite/.htpasswd:/etc/nginx/.htpasswd\
   -v /home/nikasa/Graphite/data/whisper:/opt/graphite/storage/whisper\
-  -v /home/nikasa/Graphite/configs:/opt/graphite/conf\
-  -v /home/nikasa/Graphite/statsd:/opt/statsd\
-  sitespeedio/graphite
+  -v /home/nikasa/Graphite/storage-schemas.conf:/opt/graphite/conf/storage-schemas.conf\
+  nikasa/graphite
+  #-v /home/nikasa/Graphite/statsd:/opt/statsd\
