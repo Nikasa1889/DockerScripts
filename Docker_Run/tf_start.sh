c3c115548aa78c4902da49e2f05482714c8a5bbb
Nikasa <nikasa.1889@gmail.com>
2017-09-30 11:46:14 +0200

    Add --allow-root

--- Docker_Run/tf_start.sh
+++ Docker_Run/tf_start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nvidia-docker run -it -p 8888:8888 -p 6006:6006 -p 4000:22 -v /home/nikasa/Docker/notebooks:/notebooks -v /home/nikasa/Docker/mycert.pem:/home/jovyan/.local/share/jupyter/notebook.pem -e USE_HTTPS=yes gcr.io/tensorflow/tensorflow:latest-gpu /run_jupyter.sh --NotebookApp.password='sha1:446f44ac0d61:4b45189fae99b1ff67555a4d91ddf6d7af77d84f'
+sudo nvidia-docker run -it -p 8888:8888 -p 6006:6006 -p 4000:22 -v /home/nikasa/Docker/notebooks:/notebooks -v /home/nikasa/Docker/mycert.pem:/home/jovyan/.local/share/jupyter/notebook.pem -e USE_HTTPS=yes tensorflow/tensorflow:latest-gpu /run_jupyter.sh --NotebookApp.password='sha1:446f44ac0d61:4b45189fae99b1ff67555a4d91ddf6d7af77d84f' --allow-root
